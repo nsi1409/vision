1abc94a4d2e5d43e1355a2432e7d473da92000f8
Evan <obrienem@rose-hulman.edu>
2022-07-24 17:32:08 -0400

    refactored test for autolabel format

--- test/autolabel.py
+++ test/autolabel.py
@@ -2,16 +2,16 @@ import torch
 from torchvision import transforms
 from PIL import Image
 from matplotlib import pyplot as plt
+import torchvision.autolabel as al
 
 convert_tensor = transforms.ToTensor()
-random_rotation = transforms.RandomRotation(degrees=360, angle_tuple=True)
+random_rotation = transforms.RandomRotation(degrees=360)
 
 img = Image.open('assets/encode_jpeg/grace_hopper_517x606.jpg')
 
 img = convert_tensor(img)
-img, degrees = random_rotation(img)
-
-print(degrees)
+img = al.LabelTensor(img)
+img = random_rotation(img)
 
 plt.imshow(img.permute(1, 2, 0))
 plt.show()
