d46caeb7640c90b4e5a0cf1eea95ab53ff12a0d8
Evan <obrienem@rose-hulman.edu>
2022-07-25 01:44:12 -0400

    starting LabelTensor implementation

--- torchvision/autolabel/autolabel.py
+++ torchvision/autolabel/autolabel.py
@@ -1,5 +1,6 @@
 import torch
 from .. import transforms
+import math
 
 
 class InputTensor(torch.Tensor):
@@ -11,9 +12,24 @@ class InputTensor(torch.Tensor):
 		self.transformation_manifest.append(transform)
 
 
+class MetaLabel:
+	def __init__(self, index):
+		self.index = index
+
+class X(MetaLabel):
+	pass
+
+class Y(MetaLabel):
+	pass
+
+
 class LabelTensor(torch.Tensor):
 	def __init__(self, *args, **kwargs):
+		self.internal_label = []
 		torch.Tensor.__init__(*args, **kwargs)
+
+	def meta_label(self, internal_label):
+		self.internal_label = internal_label
 
 
 class RandomRotation(transforms.RandomRotation):
@@ -21,13 +37,11 @@ class RandomRotation(transforms.RandomRotation):
 		transforms.RandomRotation.__init__(self, *args, **kwargs)
 
 	def forward(self, img):
-		print('here')
 		transformations = img.transformation_manifest
 		tensor = transforms.RandomRotation.forward(self, img)
 		input_tensor = InputTensor(tensor)
 		input_tensor.transformation_manifest = transformations
-		input_tensor.transformation_manifest.append(['rotation', self.get_params(self.degrees)])
-		print(input_tensor.transformation_manifest)
+		input_tensor.transformation_manifest.append(['rotation', (math.pi * self.get_params(self.degrees)) / 180])
 		return input_tensor
 
 
